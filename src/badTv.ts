1c38d03fb7b6f822813cdf31a5fa9b10382acb38
Luis Cislaghi <luis.cislaghi@gmail.com>
2024-11-08 21:40:50 -0300

    feat: add lens distortion filter pass

--- src/badTv.ts
+++ src/badTv.ts
@@ -5,10 +5,12 @@ import staticShader from "./shaders/tv/staticShader";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import { Pass, FullScreenQuad } from "three/addons/postprocessing/Pass.js";
 import { MaskPass } from "three/addons/postprocessing/MaskPass.js";
 import { CopyShader } from "three/addons/shaders/CopyShader.js";
 import { FilmShader } from "three/addons/shaders/FilmShader.js";
 import { RGBShiftShader } from "three/addons/shaders/RGBShiftShader.js";
+import { LensDistortionPassGen } from "three-lens-distortion";
 
 //  https://github.com/felixturner/bad-tv-shader/blob/master/example/index.html
 
@@ -20,6 +22,12 @@ export function badTvEffect(
   // console.log(badTVShader);
   // console.log(staticShader);
 
+  const LensDistortionPass = LensDistortionPassGen({
+    THREE,
+    Pass,
+    FullScreenQuad,
+  });
+
   let composer = new EffectComposer(renderer);
 
   //Create Shader Passes
@@ -29,6 +37,12 @@ export function badTvEffect(
   const filmPass = new ShaderPass(FilmShader);
   const staticPass = new ShaderPass(staticShader);
   const copyPass = new ShaderPass(CopyShader);
+  const lensDistortionPass = new LensDistortionPass({
+    distortion: new THREE.Vector2(0.01, 0.05), // radial distortion coeff
+    principalPoint: new THREE.Vector2(0, 0), // principal point coord
+    focalLength: new THREE.Vector2(0.9, 0.9), // focal length
+    skew: 0, // skew coeff
+  });
 
   //set shader uniforms
   filmPass.uniforms.grayscale.value = 0;
@@ -82,6 +96,7 @@ export function badTvEffect(
   }
 
   composer.addPass(copyPass);
+  composer.addPass(lensDistortionPass);
   copyPass.renderToScreen = true;
 
   function onParamsChange() {
