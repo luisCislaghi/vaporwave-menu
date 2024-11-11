922376379f16aa84c99412b43ff77f33be60511d
Luis Cislaghi <luis.cislaghi@gmail.com>
2024-11-11 18:01:15 -0300

    feat: organize and align menu items

--- src/text.ts
+++ src/text.ts
@@ -62,52 +62,69 @@ export async function doTextStuff() {
   });
   material.uniforms.uMap.value = atlas;
 
-  const keys = Object.keys(menuItems);
-  let lastSubGroupPosition = 0;
-  keys.forEach((key, keyIndex) => {
-    const subGroup = new THREE.Group();
-    const titleScale = 0.02;
-    const itemScale = 0.01;
+  const getTitleMesh = (text: string) => {
+    const scale = 0.02;
 
-    const title = new MSDFTextGeometry({
-      text: key,
+    const itemGeometry = new MSDFTextGeometry({
+      text: text,
+      font: font.data,
+      align: "right",
+      letterSpacing: 4,
+    });
+
+    const itemMesh = new THREE.Mesh(itemGeometry, material);
+    itemMesh.scale.set(scale, scale, scale);
+    itemMesh.rotateX(Math.PI);
+    return itemMesh;
+  };
+  const getItemMesh = (text: string) => {
+    const scale = 0.01;
+
+    const itemGeometry = new MSDFTextGeometry({
+      text: text,
       font: font.data,
       align: "right",
       letterSpacing: 10,
     });
 
-    const mesh = new THREE.Mesh(title, material);
-    mesh.position.y = -keyIndex * 2 + lastSubGroupPosition;
-    mesh.scale.set(titleScale, titleScale, titleScale);
-    mesh.rotateX(Math.PI);
+    const itemMesh = new THREE.Mesh(itemGeometry, material);
+    itemMesh.scale.set(scale, scale, scale);
+    itemMesh.rotateX(Math.PI);
+    return itemMesh;
+  };
 
+  const ITEM_MARGIN_BOTTOM = 0.6;
+  const TITLE_MARGIN_TOP = 1.5;
+  const TITLE_MARGIN_BOTTOM = 1;
+  let basePadding = 0;
+
+  Object.keys(menuItems).forEach((key, keyIndex) => {
+    if (keyIndex > 0) {
+      basePadding += TITLE_MARGIN_TOP;
+    }
+
+    const subGroup = new THREE.Group();
+
+    const mesh = getTitleMesh(key);
+    mesh.position.y = -basePadding;
     subGroup.add(mesh);
+
+    basePadding += TITLE_MARGIN_BOTTOM;
 
     const itemsGroup = new THREE.Group();
     menuItems[key].forEach((item, itemIndex) => {
-      const itemGeometry = new MSDFTextGeometry({
-        text: item,
-        font: font.data,
-        align: "right",
-        letterSpacing: 4,
-      });
-
-      const itemMesh = new THREE.Mesh(itemGeometry, material);
-      itemMesh.position.y = -itemIndex * 0.8 - 1 + lastSubGroupPosition;
-      itemMesh.scale.set(itemScale, itemScale, itemScale);
-      itemMesh.rotateX(Math.PI);
-
+      const itemMesh = getItemMesh(item);
+      itemMesh.position.y = -basePadding;
       itemsGroup.add(itemMesh);
+
+      basePadding += ITEM_MARGIN_BOTTOM;
     });
 
     subGroup.add(itemsGroup);
-    // REVER ESSE MULTIPLICADOR PARA AJUSTAR A POSIÇÃO DOS ITENS
-    // É SÓ IR SOMANDO COMO BASE DAS POSIÇÕES
-    lastSubGroupPosition -= menuItems[key].length * 0.8;
-
     group.add(subGroup);
-    group.position.z = 90;
   });
+
+  group.position.z = 90;
 
   return group;
 }
