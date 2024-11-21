7ad253f5351198b30693f4c98ce07a1835f337b8
Luis Cislaghi <luis.cislaghi@gmail.com>
2024-11-21 19:04:31 -0300

    feat: test outline and glow

--- src/menu/material.ts
+++ src/menu/material.ts
@@ -5,8 +5,12 @@ import textVertexShader from "../shaders/textVertex.glsl";
 import textFragmentShader from "../shaders/textFragment.glsl";
 
 const atlas = await new THREE.TextureLoader().loadAsync(
+  // "fonts/roboto-regular.png"
   "fonts/DelaGothicOne-Regular.png"
 );
+
+atlas.premultiplyAlpha = false;
+atlas.needsUpdate = true;
 
 const material = new THREE.ShaderMaterial({
   side: THREE.DoubleSide,
@@ -15,7 +19,7 @@ const material = new THREE.ShaderMaterial({
     IS_SMALL: false,
   },
   extensions: {
-    // derivatives: true,
+    derivatives: true,
   },
   uniforms: {
     // Common
@@ -23,13 +27,27 @@ const material = new THREE.ShaderMaterial({
 
     // Rendering
     ...uniforms.rendering,
+    // uThreshold: { value: 0.05 },
+    // uAlphaTest: { value: 0.5 },
 
     // Strokes
     ...uniforms.strokes,
+    uTextureDimentions: { value: new THREE.Vector2(1, 1) },
   },
   vertexShader: textVertexShader,
   fragmentShader: textFragmentShader,
 });
+
 material.uniforms.uMap.value = atlas;
+material.uniforms.uColor.value = new THREE.Color("#ffffff");
+material.uniforms.uStrokeColor.value = new THREE.Color("#ff0000");
+material.uniforms.uStrokeOutsetWidth.value = 0.9;
+material.uniforms.uStrokeInsetWidth.value = 0.4;
+
+console.log();
+material.uniforms.uTextureDimentions.value = new THREE.Vector2(
+  material.uniforms.uMap.value.image.width,
+  material.uniforms.uMap.value.image.height
+);
 
 export default material;
--- src/shaders/textFragment.glsl.ts
+++ src/shaders/textFragment.glsl.ts
@@ -1,67 +1,89 @@
 export default /* glsl */ `
-  // Varyings
-  varying vec2 vUv;
+// Varyings
+varying vec2 vUv;
 
-  // Uniforms: Common
-  uniform float uOpacity;
-  uniform float uThreshold;
-  uniform float uAlphaTest;
-  uniform vec3 uColor;
-  uniform sampler2D uMap;
+// Uniforms: Common
+uniform float uOpacity;
+uniform float uThreshold;
+uniform float uAlphaTest;
+uniform vec3 uColor;
+uniform sampler2D uMap;
 
-  // Uniforms: Strokes
-  uniform vec3 uStrokeColor;
-  uniform float uStrokeOutsetWidth;
-  uniform float uStrokeInsetWidth;
+// Uniforms: Strokes
+uniform vec3 uStrokeColor;
+uniform float uStrokeOutsetWidth;
+uniform float uStrokeInsetWidth;
 
-  // Utils: Median
-  float median(float r, float g, float b) {
-      return max(min(r, g), min(max(r, g), b));
-  }
+// uniform float pxRange; // set to distance field's pixel range
+uniform float uTextureDimentions;
 
-  void main() {
-      // Common
-      // Texture sample
-      vec3 s = texture2D(uMap, vUv).rgb;
+float pxRange = 2.;
 
-      // Signed distance
-      float sigDist = median(s.r, s.g, s.b) - 0.5;
+// Utils: Median
+float median(float r, float g, float b) {
+    return max(min(r, g), min(max(r, g), b));
+}
 
-      float afwidth = 1.4142135623730951 / 2.0;
+float screenPxRange(vec3 texture) {
+    vec2 unitRange = vec2(pxRange)/vec2(uTextureDimentions);
+    vec2 screenTexSize = vec2(1.0)/fwidth(vUv);
+    return max(0.5*dot(unitRange, screenTexSize), 1.0);
+}
 
-      #ifdef IS_SMALL
-          float alpha = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDist);
-      #else
-          float alpha = clamp(sigDist / fwidth(sigDist) + 0.5, 0.0, 1.0);
-      #endif
+void main() {
+    // Common
+    // Texture sample
+    vec3 s = texture2D(uMap, vUv).rgb;
 
-      // Strokes
-      // Outset
-      float sigDistOutset = sigDist + uStrokeOutsetWidth * 0.5;
+    
 
-      // Inset
-      float sigDistInset = sigDist - uStrokeInsetWidth * 0.5;
+    // Signed distance
+    float sigDist = median(s.r, s.g, s.b) - 0.5;
 
-      #ifdef IS_SMALL
-          float outset = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistOutset);
-          float inset = 1.0 - smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistInset);
-      #else
-          float outset = clamp(sigDistOutset / fwidth(sigDistOutset) + 0.5, 0.0, 1.0);
-          float inset = 1.0 - clamp(sigDistInset / fwidth(sigDistInset) + 0.5, 0.0, 1.0);
-      #endif
+    float afwidth = 1.4142135623730951 / 2.0; //1.414... == sqrt(2)
 
-      // Border
-      float border = outset * inset;
+    #ifdef IS_SMALL
+        float alpha = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDist);
+    #else
+        float alpha = clamp(sigDist / fwidth(sigDist) + 0.5, 0.0, 1.0);
+    #endif
 
-      // Alpha Test
-      if (alpha < uAlphaTest) discard;
+    // Strokes
+    // Outset
+    float sigDistOutset = sigDist + uStrokeOutsetWidth * 0.5;
 
-      // Output: Common
-      vec4 filledFragColor = vec4(uColor, uOpacity * alpha);
+    // Inset
+    float sigDistInset = sigDist - uStrokeInsetWidth * 0.5;
 
-      // Output: Strokes
-      vec4 strokedFragColor = vec4(uStrokeColor, uOpacity * border);
+    #ifdef IS_SMALL
+        float outset = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistOutset);
+        float inset = 1.0 - smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistInset);
+    #else
+        float outset = clamp(sigDistOutset / fwidth(sigDistOutset) + 0.5, 0.0, 1.0);
+        float inset = 1.0 - clamp(sigDistInset / fwidth(sigDistInset) + 0.5, 0.0, 1.0);
+    #endif
 
-      gl_FragColor = filledFragColor;
-  }
+//teste
+    float screenPxDistance = screenPxRange(s)*(sigDist - 0.5);
+    float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
+
+    // Border
+    float border = outset * inset;
+
+    // Alpha Test
+    // if (alpha < uAlphaTest) discard;
+
+    // Output: Common
+    vec4 filledFragColor = vec4(uColor, uOpacity * alpha);
+
+    gl_FragColor = filledFragColor;
+
+     // Output: Strokes
+     vec4 strokedFragColor = vec4(uStrokeColor, uOpacity * border);
+
+     gl_FragColor = mix(filledFragColor, strokedFragColor, border);
+
+    // vec4 color = (filledFragColor) + (uStrokeColor * (outer_opacity - inner_opacity));
+
+}
 `;
