f8b6433e7c8f8c67065d5fb575895dcddd47db56
Luis Cislaghi <luis.cislaghi@gmail.com>
2025-01-05 21:11:39 -0300

    feat: add header

---
+++ src/header/index.ts
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+// @ts-ignore
+import { MSDFTextGeometry } from "three-msdf-text-utils";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { jpMaterial } from "./materials";
+
+export async function renderHeader(camera: THREE.PerspectiveCamera) {
+  const group = new THREE.Group();
+  const groupZ = 95;
+  const font = await new FontLoader().loadAsync("fonts/MPLUS-medium-msdf.json");
+  // const font = await new FontLoader().loadAsync(
+  //   "fonts/DelaGothicOne-JP-msdf.json"
+  // );
+
+  // TITLE
+  const TITLE_MARGIN_TOP = 1.5;
+  const TITLE_MARGIN_BOTTOM = 1;
+  const TITLE_SCALE = 0.015;
+
+  const getMesh = (geometry: THREE.BufferGeometry, scale: number) => {
+    const mesh = new THREE.Mesh(geometry, jpMaterial);
+    mesh.scale.set(scale, scale, scale);
+    mesh.rotateX(Math.PI);
+    return mesh;
+  };
+
+  const getTextMesh = (text: string) => {
+    const itemGeometry = new MSDFTextGeometry({
+      text: text,
+      font: font.data,
+      align: "right",
+      letterSpacing: 5,
+    });
+
+    return getMesh(itemGeometry, TITLE_SCALE);
+  };
+
+  const alignToTheLeft = (mesh: THREE.Mesh) => {
+    // align to the left
+    var box = new THREE.Box3().setFromObject(mesh);
+    const size = new THREE.Vector3();
+    box.getSize(size);
+    mesh.position.x -= size.x * 2;
+  };
+
+  const titleMesh = getTextMesh("コーヒー");
+  alignToTheLeft(titleMesh);
+  group.add(titleMesh);
+  // a little gimmick to get group size
+  var box = new THREE.Box3().setFromObject(group);
+  const size = new THREE.Vector3();
+  box.getSize(size);
+
+  // get camera view size
+  let vect2 = new THREE.Vector2(0, 0);
+  camera.getViewSize(100 - groupZ, vect2);
+
+  const lensDistortionCorrection = 0.8;
+  const diff = size.x < vect2.x ? size.x / vect2.x : vect2.x / size.x;
+  const scale = diff * lensDistortionCorrection;
+  group.scale.set(scale, scale, scale);
+
+  // align to the left. add a little offset to the left
+  const lensDistortionOffset = 0.4;
+  group.position.x = vect2.x / 2 - lensDistortionOffset;
+
+  // align to the top and add a little offset to the top
+  group.position.y = vect2.y / 2 - lensDistortionOffset * 2;
+
+  group.position.z = groupZ;
+
+  return group;
+}
---
+++ src/header/materials.ts
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+// @ts-ignore
+import { uniforms } from "three-msdf-text-utils";
+import textVertexShader from "../shaders/textVertex.glsl";
+import textFragmentShader from "../shaders/textFragment.glsl";
+
+const atlas = await new THREE.TextureLoader().loadAsync(
+  "fonts/MPLUS-medium.png"
+);
+// const atlas = await new THREE.TextureLoader().loadAsync(
+//   "fonts/DelaGothicOne-JP.png"
+// );
+
+atlas.premultiplyAlpha = false;
+atlas.needsUpdate = true;
+
+const jpMaterial = new THREE.ShaderMaterial({
+  side: THREE.DoubleSide,
+  transparent: true,
+  defines: {
+    IS_SMALL: false,
+  },
+  uniforms: {
+    // Common
+    ...uniforms.common,
+
+    // Rendering
+    ...uniforms.rendering,
+    // uThreshold: { value: 0.05 },
+    // uAlphaTest: { value: 0.5 },
+
+    // Strokes
+    ...uniforms.strokes,
+
+    // Custom
+    uMap: { value: atlas },
+    uColor: { value: new THREE.Color("#ffffff") },
+    uStrokeColor: { value: new THREE.Color("#ffffff") },
+    uStrokeOutsetWidth: { value: 0.2 },
+    uStrokeInsetWidth: { value: 0.0 },
+  },
+  vertexShader: textVertexShader,
+  fragmentShader: textFragmentShader,
+});
+
+export { jpMaterial };
