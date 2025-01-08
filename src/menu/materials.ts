bdcb76b327e8b8c7eb786784daefa3c1eab5a418
Luis Cislaghi <luis.cislaghi@gmail.com>
2025-01-08 18:14:07 -0300

    refactor: code clean up

--- src/menu/materials.ts
+++ src/menu/materials.ts
@@ -5,66 +5,37 @@ import textVertexShader from "../shaders/textVertex.glsl";
 import textFragmentShader from "../shaders/textFragment.glsl";
 
 const atlas = await new THREE.TextureLoader().loadAsync("fonts/Radwave.png");
-
 atlas.premultiplyAlpha = false;
 atlas.needsUpdate = true;
 
-const titleMaterial = new THREE.ShaderMaterial({
+const defaultUnfiforms = {
+  // Common
+  ...uniforms.common,
+  // Rendering
+  ...uniforms.rendering,
+  // Strokes
+  ...uniforms.strokes,
+  // Custom
+  uMap: { value: atlas },
+  uColor: { value: new THREE.Color("#ffffff") },
+  uStrokeColor: { value: new THREE.Color("#ffffff") },
+  uStrokeOutsetWidth: { value: 0.2 },
+  uStrokeInsetWidth: { value: 0.0 },
+};
+
+const materialConfig = {
   side: THREE.DoubleSide,
   transparent: true,
   defines: {
     IS_SMALL: false,
   },
-  uniforms: {
-    // Common
-    ...uniforms.common,
-
-    // Rendering
-    ...uniforms.rendering,
-    // uThreshold: { value: 0.05 },
-    // uAlphaTest: { value: 0.5 },
-
-    // Strokes
-    ...uniforms.strokes,
-
-    // Custom
-    uMap: { value: atlas },
-    uColor: { value: new THREE.Color("#ffffff") },
-    uStrokeColor: { value: new THREE.Color("#ffffff") },
-    uStrokeOutsetWidth: { value: 0.2 },
-    uStrokeInsetWidth: { value: 0.0 },
-  },
+  uniforms: defaultUnfiforms,
   vertexShader: textVertexShader,
   fragmentShader: textFragmentShader,
-});
+};
 
-const bodyMaterial = new THREE.ShaderMaterial({
-  side: THREE.DoubleSide,
-  transparent: true,
-  defines: {
-    IS_SMALL: false,
-  },
-  uniforms: {
-    // Common
-    ...uniforms.common,
-
-    // Rendering
-    ...uniforms.rendering,
-    // uThreshold: { value: 0.05 },
-    // uAlphaTest: { value: 0.5 },
-
-    // Strokes
-    ...uniforms.strokes,
-
-    // Custom
-    uMap: { value: atlas },
-    uColor: { value: new THREE.Color("#ffffff") },
-    uStrokeColor: { value: new THREE.Color("#ffffff") },
-    uStrokeOutsetWidth: { value: 0.2 },
-    uStrokeInsetWidth: { value: 0.0 },
-  },
-  vertexShader: textVertexShader,
-  fragmentShader: textFragmentShader,
-});
+// yes, they are the same material. I let it this way because I may want to change it later
+const titleMaterial = new THREE.ShaderMaterial(materialConfig);
+const bodyMaterial = new THREE.ShaderMaterial(materialConfig);
 
 export { titleMaterial, bodyMaterial };
