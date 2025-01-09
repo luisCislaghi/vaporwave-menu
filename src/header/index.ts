b76256670671ee7911dede2ba047eb65489853ae
Luis Cislaghi <luis.cislaghi@gmail.com>
2025-01-09 17:27:39 -0300

    feat: wip header

--- src/header/index.ts
+++ src/header/index.ts
@@ -2,53 +2,65 @@ import * as THREE from "three";
 // @ts-ignore
 import { MSDFTextGeometry } from "three-msdf-text-utils";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
-import { jpMaterial } from "./materials";
+import { radwaveMaterial, mplusMaterial } from "./materials";
 
 export async function drawHeader(
   scene: THREE.Scene,
   camera: THREE.PerspectiveCamera
 ) {
   const group = new THREE.Group();
-  const groupZ = 95;
-  const font = await new FontLoader().loadAsync("fonts/MPLUS-medium-msdf.json");
-  // const font = await new FontLoader().loadAsync(
-  //   "fonts/DelaGothicOne-JP-msdf.json"
-  // );
+  const groupZ = 94;
+  const fontMplus = await new FontLoader().loadAsync(
+    "fonts/MPLUS-medium-msdf.json"
+  );
+  const fontRadwave = await new FontLoader().loadAsync(
+    "fonts/Radwave-msdf.json"
+  );
 
   // TITLE
-  const TITLE_MARGIN_TOP = 1.5;
-  const TITLE_MARGIN_BOTTOM = 1;
-  const TITLE_SCALE = 0.017;
+  const JP_SCALE = 0.017;
+  const LATIM_SCALE = 0.012;
 
-  const getMesh = (geometry: THREE.BufferGeometry, scale: number) => {
-    const mesh = new THREE.Mesh(geometry, jpMaterial);
+  const getTextMesh = (
+    geometry: THREE.BufferGeometry,
+    scale: number,
+    material: THREE.ShaderMaterial
+  ) => {
+    const mesh = new THREE.Mesh(geometry, material);
     mesh.scale.set(scale, scale, scale);
     mesh.rotateX(Math.PI);
     return mesh;
   };
 
-  const getTextMesh = (text: string) => {
+  const getJPTextMesh = (text: string) => {
     const geometry = new MSDFTextGeometry({
       text: text,
-      font: font.data,
+      font: fontMplus.data,
       align: "right",
       letterSpacing: 5,
     });
 
-    return getMesh(geometry, TITLE_SCALE);
+    return getTextMesh(geometry, JP_SCALE, mplusMaterial);
   };
 
-  const alignToTheLeft = (mesh: THREE.Mesh) => {
-    // align to the left
-    var box = new THREE.Box3().setFromObject(mesh);
-    const size = new THREE.Vector3();
-    box.getSize(size);
-    mesh.position.x -= size.x;
+  const getLatinTextMesh = (text: string) => {
+    const geometry = new MSDFTextGeometry({
+      text: text,
+      font: fontRadwave.data,
+      align: "right",
+      letterSpacing: 5,
+    });
+
+    return getTextMesh(geometry, LATIM_SCALE, radwaveMaterial);
   };
 
-  const titleMesh = getTextMesh("コーヒー");
-  alignToTheLeft(titleMesh);
-  group.add(titleMesh);
+  const jpMesh = getJPTextMesh("コーヒー");
+  // jpMesh.position.x += 0.5;
+  group.add(jpMesh);
+  const latinMesh = getLatinTextMesh("COFFEE");
+  latinMesh.position.x += 0.13;
+  latinMesh.position.y -= 0.6;
+  group.add(latinMesh);
 
   // a little gimmick to get group size
   var box = new THREE.Box3().setFromObject(group);
@@ -59,14 +71,14 @@ export async function drawHeader(
   let vect2 = new THREE.Vector2(0, 0);
   camera.getViewSize(100 - groupZ, vect2);
 
-  const lensDistortionCorrection = 0.8;
+  const lensDistortionCorrection = 1;
   const diff = size.x < vect2.x ? size.x / vect2.x : vect2.x / size.x;
   const scale = diff * lensDistortionCorrection;
   group.scale.set(scale, scale, scale);
 
-  // align to the right. add a little offset to the right
-  const lensDistortionOffset = 0.45;
-  // group.position.x = vect2.x / 2 - lensDistortionOffset;
+  // align to the left and add a little offset to the left
+  const lensDistortionOffset = 0.5;
+  group.position.x -= vect2.x / 2 - lensDistortionOffset;
 
   // align to the top and add a little offset to the top
   group.position.y = vect2.y / 2 - lensDistortionOffset * 2;
--- src/header/materials.ts
+++ src/header/materials.ts
@@ -4,43 +4,55 @@ import { uniforms } from "three-msdf-text-utils";
 import textVertexShader from "../shaders/textVertex.glsl";
 import textFragmentShader from "../shaders/textFragment.glsl";
 
-const atlas = await new THREE.TextureLoader().loadAsync(
+const atlasMPLUS = await new THREE.TextureLoader().loadAsync(
   "fonts/MPLUS-medium.png"
 );
-// const atlas = await new THREE.TextureLoader().loadAsync(
-//   "fonts/DelaGothicOne-JP.png"
-// );
+const atlasRadwave = await new THREE.TextureLoader().loadAsync(
+  "fonts/Radwave.png"
+);
 
-atlas.premultiplyAlpha = false;
-atlas.needsUpdate = true;
+atlasMPLUS.premultiplyAlpha = false;
+atlasMPLUS.needsUpdate = true;
+atlasRadwave.premultiplyAlpha = false;
+atlasRadwave.needsUpdate = true;
 
-const jpMaterial = new THREE.ShaderMaterial({
+const defaultUnfiforms = {
+  // Common
+  ...uniforms.common,
+  // Rendering
+  ...uniforms.rendering,
+  // Strokes
+  ...uniforms.strokes,
+  // Custom
+  uMap: { value: atlasRadwave },
+  uColor: { value: new THREE.Color("#b3e6e1") },
+  uStrokeColor: { value: new THREE.Color("#b3e6e1") },
+  uStrokeOutsetWidth: { value: 0.2 },
+  uStrokeInsetWidth: { value: 0.0 },
+  // uOpacity: { value: 0.1 },
+};
+
+const materialConfig = {
   side: THREE.DoubleSide,
   transparent: true,
   defines: {
     IS_SMALL: false,
   },
-  uniforms: {
-    // Common
-    ...uniforms.common,
-
-    // Rendering
-    ...uniforms.rendering,
-    // uThreshold: { value: 0.05 },
-    // uAlphaTest: { value: 0.5 },
-
-    // Strokes
-    ...uniforms.strokes,
-
-    // Custom
-    uMap: { value: atlas },
-    uColor: { value: new THREE.Color("#ffffff") },
-    uStrokeColor: { value: new THREE.Color("#ffffff") },
-    uStrokeOutsetWidth: { value: 0.2 },
-    uStrokeInsetWidth: { value: 0.0 },
-  },
+  uniforms: defaultUnfiforms,
   vertexShader: textVertexShader,
   fragmentShader: textFragmentShader,
+};
+
+// yes, they are the same material. I let it this way because I may want to change it later
+const radwaveMaterial = new THREE.ShaderMaterial(materialConfig);
+const mplusMaterial = new THREE.ShaderMaterial({
+  ...materialConfig,
+  uniforms: {
+    ...materialConfig.uniforms,
+    uMap: {
+      value: atlasMPLUS,
+    },
+  },
 });
 
-export { jpMaterial };
+export { radwaveMaterial, mplusMaterial };
